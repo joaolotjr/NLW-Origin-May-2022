12ab0f49e63ab70eab3b0c79afe24d6026bd8925
joaolotjr <joaolotjr@gmail.com>
2022-05-14 17:38:23 -0300

    ADD Contact and About Topics

--- main.js
+++ main.js
@@ -3,11 +3,26 @@
 // Number
 // Boolean (true|false)
 
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnSroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+function showBackToTopButtonOnSroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -19,4 +34,17 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-ScrollReveal().reveal('#home')
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card
+#about,
+#about header,
+#about .content`)
